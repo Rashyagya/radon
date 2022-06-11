33b3969893878b4da1e19c59505c8575f7a7c285
rashmi <rashy0405@gmail.com>
2022-06-11 17:21:45 +0530

    middleware 1

--- src/index.js
+++ src/index.js
@@ -7,16 +7,52 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const midGlb= function (req, res, next) {
+    console.log("Hi I am a GlOBAL middleware");
+    //logic
+    next()
+}
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+const assignmentMW= function (req, res, next) {
+
+  // current date and time                 
+    let date_ob = new Date();
+let date = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let year = date_ob.getFullYear();
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let seconds = date_ob.getSeconds();
+console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds)
+
+    // for ip address
+    const dns = require('dns');
+dns.lookup('www.google.org', 
+(err, addresses, family) => {
+   console.log(addresses);
+    //console.log("/creatUser")
+
+ console.log("/creatUser")
+    //next();
+})
+
+    
+    //console.log('$(datetime)  $(ip)  $(url)')
+                   
+}
+
+app.use(assignmentMW)
+
+mongoose.connect("mongodb+srv://Rashmivishwakarma:rashmi1996@cluster0.m1asu.mongodb.net/10junework?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
+.then( () => console.log("Your MongoDb is connected"))
 .catch ( err => console.log(err) )
 
 app.use (
     function (req, res, next) {
         console.log ("inside GLOBAL MW");
+        //req.body ("inside GLOBAL MW");
         next();
   }
   );
